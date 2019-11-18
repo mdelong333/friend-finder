c2daed80c5a00788991056edac764e19f8c137f5
murnda <mdelong333@gmail.com>
2019-11-17 22:48:53 -0500

    submit button saves user input into newfriend variable and adds to friends api, commented out sections of question code to shorten testing

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+    {
+        name: "Miranda",
+        imageLink: "https://stuffhappens.us/wp-content/uploads/2015/01/fish-10.jpg",
+        ratings: [
+            5,
+            5,
+            // 1,
+            // 4,
+            // 5,
+            // 2,
+            // 3,
+            // 3,
+            // 3,
+            // 1
+        ],
+    }
+];
+
+module.exports = friendsArray;
